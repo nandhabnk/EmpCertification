8938e53fb4cc77dc8574ad1aa595577d6f001389
nandhabnk <68983586+nandhabnk@users.noreply.github.com>
2024-04-11 15:35:22 +0530

    feat: Added PDF viewer in request details

--- src/Pages/AllRequestsPage/Components/RequestDetails/RequestDetails.tsx
+++ src/Pages/AllRequestsPage/Components/RequestDetails/RequestDetails.tsx
@@ -1,8 +1,13 @@
-import { Box, Card, Grid, Typography, Divider } from "@mui/material";
+import { PDFViewer } from "@react-pdf/renderer";
+
+import { Box, Card, Grid, Typography, Divider, Stack } from "@mui/material";
+
+import InfoIcon from "@mui/icons-material/Info";
 
 import { RequestData } from "../../../../shared/types/requestDetails";
-
 import KeyValuePair from "../../../../shared/Components/KeyValuePair";
+
+import RequestPdfDoc from "../RequestPdfDoc";
 
 import * as Styled from "./RequestDetails.style";
 
@@ -25,21 +30,44 @@ const RequestDetails = ({
     <Styled.RequestDetailsWrapper>
       <Card variant="outlined">
         <Box>
-          <Grid container flexDirection="column">
-            <Typography
-              variant="h4"
-              component="h2"
-              textAlign={"center"}
-              id="request-details-heading"
-            >
-              Certificate request details
-            </Typography>
-            <Divider />
-            {Object.entries(sortedCurrentRequest(currentRequest)).map(
-              (data, i) => (
-                <KeyValuePair key={i} data={data} />
-              )
-            )}
+          <Typography
+            variant="h4"
+            component="h2"
+            textAlign={"center"}
+            id="request-details-heading"
+          >
+            Certificate request details
+          </Typography>
+          <Grid
+            container
+            flexDirection="row"
+            justifyContent="space-around"
+            padding={"10px"}
+          >
+            <Grid item xs={6}>
+              {Object.entries(sortedCurrentRequest(currentRequest)).map(
+                (data) => (
+                  <div key={data[0]}>
+                    <KeyValuePair data={data} />
+                    <Divider />
+                  </div>
+                )
+              )}
+            </Grid>
+            <Grid item xs={4} alignContent="space-around">
+              {currentRequest.status.toLowerCase() === "done" ? (
+                <PDFViewer>
+                  <RequestPdfDoc currentRequest={currentRequest} />
+                </PDFViewer>
+              ) : (
+                <Stack alignItems="center">
+                  <InfoIcon fontSize="large" color="primary" />
+                  <Typography variant="h4" component="h2" textAlign={"center"}>
+                    Certificate is yet to be issued.
+                  </Typography>
+                </Stack>
+              )}
+            </Grid>
           </Grid>
         </Box>
       </Card>
